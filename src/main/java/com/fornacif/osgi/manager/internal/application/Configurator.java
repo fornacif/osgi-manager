e76ef0bda4ceca85d8c394bfcf9e4d2d8a58364a
Francois Fornaciari <francois.fornaciari@gmail.com>
2012-10-17 08:46:45 +0200

    Made the Configurator generic

--- src/main/java/com/fornacif/osgi/manager/internal/application/Configurator.java
+++ src/main/java/com/fornacif/osgi/manager/internal/application/Configurator.java
@@ -1,9 +1,13 @@
 package com.fornacif.osgi.manager.internal.application;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Properties;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.wiring.BundleWiring;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 
@@ -13,15 +17,7 @@ import aQute.bnd.annotation.component.Reference;
 
 @Component(name = "Configurator")
 public class Configurator {
-	private final static String LAUNCHER_CONFIGURATION_NAME = "Launcher";
-	private final static String LAUNCHER_CONFIGURATION_FILENAME = "/configurations/Launcher.properties";
-
-	private final static String BUNDLES_CONTROLLER_CONFIGURATION_NAME = "BundlesController";
-	private final static String BUNDLES_CONTROLLER_CONFIGURATION_FILENAME = "/configurations/BundlesController.properties";
-
-	private final static String SERVICES_CONTROLLER_CONFIGURATION_NAME = "ServicesController";
-	private final static String SERVICES_CONTROLLER_CONFIGURATION_FILENAME = "/configurations/ServicesController.properties";
-
+	private final static String CONFIGURATION_EXTENSION = ".properties";
 	private ConfigurationAdmin configurationAdmin;
 
 	@Reference
@@ -30,16 +26,20 @@ public class Configurator {
 	}
 
 	@Activate
-	public void activate() throws IOException {
-		configure(LAUNCHER_CONFIGURATION_NAME, LAUNCHER_CONFIGURATION_FILENAME);
-		configure(BUNDLES_CONTROLLER_CONFIGURATION_NAME, BUNDLES_CONTROLLER_CONFIGURATION_FILENAME);
-		configure(SERVICES_CONTROLLER_CONFIGURATION_NAME, SERVICES_CONTROLLER_CONFIGURATION_FILENAME);
+	public void activate(BundleContext bundleContext) throws IOException {
+		BundleWiring bundleWiring = bundleContext.getBundle().adapt(BundleWiring.class);
+		Collection<String> configurations = bundleWiring.listResources("/configurations", "*" + CONFIGURATION_EXTENSION, BundleWiring.LISTRESOURCES_LOCAL);
+		for (String configuration : configurations) {
+			configure(configuration);
+		}
 	}
 
-	private void configure(String fileName, String configurationName) throws IOException {
-		Configuration configuration = this.configurationAdmin.getConfiguration(fileName, null);
+	private void configure(String configurationPath) throws IOException {
+		String configurationFileName = new File(configurationPath).getName();
+		String configurationName = configurationFileName.substring(0, configurationFileName.length() - CONFIGURATION_EXTENSION.length());
+		Configuration configuration = this.configurationAdmin.getConfiguration(configurationName, null);
 		Properties properties = new Properties();
-		properties.load(getClass().getResourceAsStream(configurationName));
+		properties.load(getClass().getResourceAsStream("/" + configurationPath));
 		configuration.update(new Hashtable(properties));
 	}
 }
