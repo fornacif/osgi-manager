b8ab8b599c637d15fdc53c18dc3b326e55143acd
Francois Fornaciari <francois.fornaciari@gmail.com>
2012-10-18 12:57:27 +0200

    Complete the retrieval of bundles through JMX

--- src/main/java/com/fornacif/osgi/manager/internal/services/BundlesServiceImpl.java
+++ src/main/java/com/fornacif/osgi/manager/internal/services/BundlesServiceImpl.java
@@ -5,11 +5,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import javax.management.JMX;
-import javax.management.MBeanServerConnection;
 import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.CompositeData;
 import javax.management.openmbean.TabularData;
 
 import org.osgi.jmx.framework.BundleStateMBean;
@@ -47,6 +44,7 @@ public class BundlesServiceImpl implements BundlesService {
 	@Override
 	public void executeAction(Action action, Long bundleId) throws IOException {
 		FrameworkMBean mbeanProxy = jmxService.getFrameworkMBean();
+		
 		LOGGER.debug("{} {}", action, bundleId);
 		
 		switch (action) {
@@ -65,22 +63,33 @@ public class BundlesServiceImpl implements BundlesService {
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
 	private List<BundleModel> buildModel(TabularData bundleData) {
 		List<BundleModel> bundles = new ArrayList<>();
-		Collection<CompositeDataSupport> values = (Collection<CompositeDataSupport>) bundleData.values();
-		for (CompositeDataSupport compositeDataSupport : values) {
-			Long id = (Long) compositeDataSupport.get(BundleStateMBean.IDENTIFIER);
-			String symbolicName = (String) compositeDataSupport.get(BundleStateMBean.SYMBOLIC_NAME);
-			String version = (String) compositeDataSupport.get(BundleStateMBean.VERSION);
-			String state = (String) compositeDataSupport.get(BundleStateMBean.STATE);
-			Integer startLevel = (Integer) compositeDataSupport.get(BundleStateMBean.START_LEVEL);
-			
+		Collection<CompositeData> bundlesCompositeData = (Collection<CompositeData>) bundleData.values();
+		for (CompositeData bundleCompositeData : bundlesCompositeData) {
 			BundleModel bundleModel = new BundleModel();
+			
+			Long id = (Long) bundleCompositeData.get(BundleStateMBean.IDENTIFIER);
+			String symbolicName = (String) bundleCompositeData.get(BundleStateMBean.SYMBOLIC_NAME);
+			String version = (String) bundleCompositeData.get(BundleStateMBean.VERSION);
+			String state = (String) bundleCompositeData.get(BundleStateMBean.STATE);
+			Integer startLevel = (Integer) bundleCompositeData.get(BundleStateMBean.START_LEVEL);
+			TabularData headers = (TabularData) bundleCompositeData.get(BundleStateMBean.HEADERS);
+			
 			bundleModel.setId(id);
 			bundleModel.setSymbolicName(symbolicName);
 			bundleModel.setVersion(version);
 			bundleModel.setState(state);
 			bundleModel.setStartLevel(startLevel);
+			
+			Collection<CompositeData> headersCompositeData = (Collection<CompositeData>) headers.values();
+			for (CompositeData headerCompositeData : headersCompositeData) {
+				if (headerCompositeData.containsValue("Bundle-Name")) {
+					String name= (String) headerCompositeData.get("Value");
+					bundleModel.setName(name);
+				}
+			}
 			
 			bundles.add(bundleModel);
 		}
