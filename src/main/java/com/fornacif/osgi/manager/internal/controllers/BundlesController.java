826cda51704537ce87b390072717fff1ff6e2e88
Francois Fornaciari <francois.fornaciari@gmail.com>
2012-10-18 16:36:04 +0200

    Add bundle name filter

--- src/main/java/com/fornacif/osgi/manager/internal/controllers/BundlesController.java
+++ src/main/java/com/fornacif/osgi/manager/internal/controllers/BundlesController.java
@@ -3,6 +3,7 @@ package com.fornacif.osgi.manager.internal.controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -12,6 +13,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
@@ -26,6 +29,8 @@ import com.fornacif.osgi.manager.internal.events.BundleActionEvent;
 import com.fornacif.osgi.manager.internal.events.BundleActionEvent.Action;
 import com.fornacif.osgi.manager.internal.models.BundleModel;
 import com.fornacif.osgi.manager.services.BundlesService;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 
 @Component(name = "BundlesController", provide = { Pane.class }, configurationPolicy = ConfigurationPolicy.require)
 public class BundlesController extends VBox implements Initializable {
@@ -34,11 +39,16 @@ public class BundlesController extends VBox implements Initializable {
 	private BundlesService bundlesService;
 
 	@FXML
-	private TableView<BundleModel> bundlesTableView;
+	private Label bundlesCountLabel;
 
 	@FXML
-	private Label bundlesLabel;
+	private TableView<BundleModel> bundlesTableView;
 	
+	@FXML
+	private TextField filterTextField;
+	
+	private List<BundleModel> unfilteredBundles = new ArrayList<>();
+
 	@Reference
 	public void bindBundlesService(BundlesService bundlesService) {
 		this.bundlesService = bundlesService;
@@ -53,27 +63,45 @@ public class BundlesController extends VBox implements Initializable {
 		List<BundleModel> bundles = new ArrayList<>();
 		try {
 			bundles = bundlesService.listBundles();
+			unfilteredBundles.clear();
+			unfilteredBundles.addAll(bundles);
+			updateTableView(bundles);
+			bundlesCountLabel.setText(String.valueOf(bundles.size()));
 		} catch (IOException e) {
 			LOGGER.error("Error during loading bundles", e);
-		}
-		
-		bundlesTableView.getItems().setAll(bundles);
+		}		
+	}
 
+	
+	@FXML
+	protected void filterBundles(KeyEvent keyEvent) {
+		Predicate<BundleModel> predicate = new Predicate<BundleModel>() {
+			public boolean apply(BundleModel bundleModel) {
+				if (!filterTextField.getText().equals("")) {
+					return bundleModel.getName().toUpperCase().contains(filterTextField.getText().toUpperCase());
+				} 
+				return true;	
+			}
+		};
+		Collection<BundleModel> filteredBundles = Collections2.filter(unfilteredBundles, predicate);
+		updateTableView(filteredBundles);
+	}
+
+	@FXML
+	protected void executeBundleAction(BundleActionEvent bundleActionEvent) throws IOException {
+		Action action = bundleActionEvent.getAction();
+		Long bundleId = bundleActionEvent.getBundleId();
+		bundlesService.executeAction(action, bundleId);
+		loadBundles();
+	}
+	
+	private void updateTableView(Collection<BundleModel> filteredBundles) {
+		bundlesTableView.getItems().setAll(filteredBundles);
 		ObservableList<TableColumn<BundleModel, ?>> sortOrder = bundlesTableView.getSortOrder();
 		if (sortOrder.size() > 0) {
 			TableColumn<BundleModel, ?> sortedTableColumn = sortOrder.get(0);
 			sortOrder.clear();
 			sortOrder.add(sortedTableColumn);
 		}
-
-		bundlesLabel.setText("Informations: " + bundles.size() + " bundles");
-	}
-
-	@FXML
-	protected void handleBundleAction(BundleActionEvent bundleActionEvent) throws IOException {
-		Action action = bundleActionEvent.getAction();
-		Long bundleId = bundleActionEvent.getBundleId();
-		bundlesService.executeAction(action, bundleId);
-		loadBundles();
 	}
 }
