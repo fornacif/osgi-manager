98b6d4d820e2be41c9eb5be1634a0d586fe2ddfc
fornacif <francois.fornaciari@gmail.com>
2013-05-16 16:52:43 +0200

    Minor windows executable improvement

--- assembly/src/main/resources/bin/equinox.bat
+++ assembly/src/main/resources/bin/equinox.bat
@@ -1,11 +1,11 @@
 @echo off
 
+SET DIR=%~dp0
 set JDK_HOME=D:/Program Files/Java/jdk1.7.0_21
-
 set PATH="%JDK_HOME%/bin"
 
 SET CLASSPATH="%JDK_HOME%/jre/lib/jfxrt.jar"
 SET CLASSPATH=%CLASSPATH%;"%JDK_HOME%/lib/tools.jar"
-SET CLASSPATH=%CLASSPATH%;../org.eclipse.osgi-${equinox.version}.jar
+SET CLASSPATH=%CLASSPATH%;"%DIR%/../org.eclipse.osgi-${equinox.version}.jar"
 
 "%JDK_HOME%/bin/java.exe" -Dlogback.configurationFile=../configuration/logback.xml -cp %CLASSPATH% org.eclipse.core.runtime.adaptor.EclipseStarter -console
