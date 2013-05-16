178bb56f25a26dde371e97e7229ae17be79af0a1
fornacif <francois.fornaciari@gmail.com>
2013-05-16 16:04:17 +0200

    Improve windows script

--- assembly/src/main/resources/bin/equinox.bat
+++ assembly/src/main/resources/bin/equinox.bat
@@ -1,1 +1,11 @@
-java -Dlogback.configurationFile=../configuration/logback.xml -cp ../org.eclipse.osgi-${equinox.version}.jar;"C:/Program Files/Java/jre7/lib"/jfxrt.jar org.eclipse.core.runtime.adaptor.EclipseStarter -console
+@echo off
+
+set JDK_HOME=D:/Program Files/Java/jdk1.7.0_21
+
+set PATH="%JDK_HOME%/bin"
+
+SET CLASSPATH="%JDK_HOME%/jre/lib/jfxrt.jar"
+SET CLASSPATH=%CLASSPATH%;"%JDK_HOME%/lib/tools.jar"
+SET CLASSPATH=%CLASSPATH%;../org.eclipse.osgi-${equinox.version}.jar
+
+"%JDK_HOME%/bin/java.exe" -Dlogback.configurationFile=../configuration/logback.xml -cp %CLASSPATH% org.eclipse.core.runtime.adaptor.EclipseStarter -console
